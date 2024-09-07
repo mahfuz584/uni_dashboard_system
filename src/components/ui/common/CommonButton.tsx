27c9e30552e98a0113417a8a7d06100423e5ec2d
Mahfuz-Hasaan <mahfuzhasan587@gmail.com>
2024-09-08 00:23:36 +0600

    form type added in offcanvas

--- src/components/ui/common/CommonButton.tsx
+++ src/components/ui/common/CommonButton.tsx
@@ -12,8 +12,8 @@ const CommonButton: React.FC<TButton> = ({ variant, title }) => {
     switch (variant) {
       case "primary":
         return (
-          <button className="bg-[#112a41]" type="submit">
-            <p>{title}</p>
+          <button className="bg-[#112a41] rounded-md" type="submit">
+            <p className="text-white px-3 py-2">{title}</p>
           </button>
         );
       case "secondary":
--- src/pages/admin/academicManagemnt/AcademicSemester.tsx
+++ src/pages/admin/academicManagemnt/AcademicSemester.tsx
@@ -5,11 +5,61 @@ import { useAppSelector } from "redux/hooks";
 
 const AcademicSemester = () => {
   const { open } = useAppSelector((state) => state.offcanvas);
+
+  //form input fields
+  const formFields = [
+    {
+      label: "Semester Name",
+      type: "text",
+      placeholder: "Enter Semester Name",
+      name: "name",
+      rules: {
+        required: "Semester Name is required",
+      },
+    },
+    {
+      label: "Semester Code",
+      type: "text",
+      placeholder: "Enter Semester Code",
+      name: "code",
+      rules: {
+        required: "Semester Name is required",
+      },
+    },
+    {
+      label: "Academic Year",
+      type: "year",
+      placeholder: "Enter Academic Year",
+      name: "year",
+      rules: {
+        required: "Semester Name is required",
+      },
+    },
+    {
+      label: "Start Month",
+      type: "month",
+      placeholder: "Enter Start Month",
+      name: "startMonth",
+      rules: {
+        required: "Semester Name is required",
+      },
+    },
+    {
+      label: "End Month",
+      type: "month",
+      placeholder: "Enter End Month",
+      name: "endMonth",
+      rules: {
+        required: "Semester Name is required",
+      },
+    },
+  ];
+
   return (
     <>
       <TitleAndBtn title="Academic Semester" />
       <DataTable />
-      <Offcanvas open={open} />
+      <Offcanvas open={open} formFields={formFields} />
     </>
   );
 };
